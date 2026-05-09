80f12e868a03381bc45615cdaedc0daf80b59d27
unknown <gwilda77@gmail.com>
2026-05-10 00:30:53 +0700

    Initial commit

---
+++ Driver.java
@@ -0,0 +1,8 @@
+
+public class Driver { 
+    public static void main(String[] args) {
+        Shouter shouter = new Shouter();
+
+        shouter.shout("Hurrah");
+    }
+}
---
+++ Shouter.java
@@ -0,0 +1,7 @@
+public class Shouter {
+    public void shout(String message) {
+        String shoutMessage = message.toUpperCase() + "!";
+
+        System.out.println(shoutMessage);
+    }
+}
